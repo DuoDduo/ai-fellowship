2ce779004076f049d5a8ca8a091c3d1f87c09d44
blessing <pelubs2956@gmail.com>
2025-09-08 12:36:22 +0100

    setup

---
+++ WeekOne/practice.py
@@ -0,0 +1,13 @@
+# This is a code simulating how to check your data balance 
+# print(" Data Balance Check\n To check your data balance kindly dial *323#")
+# print(input(" Type/Dial *323#: "))
+# print("Enter number between 1-3 to choose an option \n1. Access Plan Balance \n2. Business Plan Balance \n3. Broadband balance \n")
+# print(int(input("Enter number: ")))
+# print("Your data balance is 2.5GB \n ")
+
+# This is a code simulating bank services 
+print(" Welcome to Diamond Bank\n Kindly Choose a service \n")
+print("To choose a service enter number between 1-4 \n1.Buy Data/Airtime\n2.Deposit money \n3.Withdraw  money\n4.Make Transfer")
+print(int(input("Enter number: ")))
+
+print("The next steps for your selected service has been sent to your email, please check back shortly... ")
